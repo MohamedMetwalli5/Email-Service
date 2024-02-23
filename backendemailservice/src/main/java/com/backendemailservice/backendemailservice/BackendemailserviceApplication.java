55eae14c191133eefd4cb821cd7081d51da95ec0
MohamedMetwalli5 <mohamedmewtalli5@gmail.com>
2024-02-23 21:13:58 +0200

    Adding some updates

---
+++ backendemailservice/src/main/java/com/backendemailservice/backendemailservice/BackendemailserviceApplication.java
@@ -0,0 +1,13 @@
+package com.backendemailservice.backendemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendemailserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendemailserviceApplication.class, args);
+	}
+
+}
---
+++ backendemailservice/src/test/java/com/backendemailservice/backendemailservice/BackendemailserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendemailservice.backendemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendemailserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
