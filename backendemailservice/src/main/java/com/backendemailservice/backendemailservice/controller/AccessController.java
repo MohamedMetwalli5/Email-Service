9cf2f88b31e57d595e2a61429085437a7401e899
MohamedMetwalli5 <mohamedmewtalli5@gmail.com>
2024-02-24 02:06:50 +0200

    Adding some updates

--- backendemailservice/src/main/java/com/backendemailservice/backendemailservice/controller/AccessController.java
+++ backendemailservice/src/main/java/com/backendemailservice/backendemailservice/controller/AccessController.java
@@ -1,10 +1,26 @@
 package com.backendemailservice.backendemailservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.catalina.connector.Response;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backendemailservice.backendemailservice.entity.User;
+import com.backendemailservice.backendemailservice.service.UserService;
 
 @RestController
 public class AccessController {
+	
+	@PostMapping("/") 
+	public Response signin(User user) {
+		// TODO logic to use an implemented user service to check on the DB for the existence of a user with both email and password
+		return UserService.findUser(user);	
+	}
+	
+	@PostMapping("/signup") 
+	public Response signup(User user) {
+		// TODO logic to use an implemented user service to check on the DB for the existence of a user with this email
+		return UserService.createUser(user);	
+	}
 	
 	
 	
---
+++ backendemailservice/src/main/java/com/backendemailservice/backendemailservice/service/UserService.java
@@ -0,0 +1,19 @@
+package com.backendemailservice.backendemailservice.service;
+
+import org.apache.catalina.connector.Response;
+
+import com.backendemailservice.backendemailservice.entity.User;
+
+public class UserService {
+
+	public static Response createUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Response findUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
